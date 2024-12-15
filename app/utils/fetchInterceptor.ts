b148721472585a5a2de113d900755ffb1e2216da
Alexander Teglund <alexteg@gmail.com>
2024-12-15 16:58:43 +0100

    Create simplified fetchApi that includes base URL
    This way one doesn't have to prepend every URL with
    `${process.env.NEXT_PUBLIC_APIBASE}/`.

--- app/utils/fetchInterceptor.ts
+++ app/utils/fetchInterceptor.ts
@@ -3,15 +3,18 @@ import { cookies } from "next/headers";
 const fetchInterceptor = async (url: string) => {
   const jwt = await (await cookies()).get("jwt")
   const data = await fetch(url, {
-      headers: {
-    Accept: "application/json",
-    Authorization:
-      "Bearer " + jwt?.value ,
-  },
-  cache: "no-store"
+    headers: {
+      Accept: "application/json",
+      Authorization:
+        "Bearer " + jwt?.value ,
+    },
+    cache: "no-store"
   });
   const res = await data.json();
   return (res);
 }
 
 export default fetchInterceptor;
+
+export const fetchApi = (url: string) =>
+  fetchInterceptor(`${process.env.NEXT_PUBLIC_APIBASE}${url}`);
