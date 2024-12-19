fce50f6f0b2e3084c02672309101ee0b36f60acf
Alexander Teglund <alexteg@gmail.com>
2024-12-19 23:19:53 +0100

    Add error handling to fetch & enable client use
    This adds various error handling to the fetchApi/Interceptor, such as
    throwing errors if the server returns an error and showing a toaster
    message with the error. However, client errors are just thrown and need
    to be handled on a case-by-case basis, depending on how the error should
    be presented to the user.
    
    This also enables using the `fetchApi` in client components, as the
    isomorphic cookie utility is used.

---
+++ app/utils/api/errors.ts
@@ -0,0 +1,20 @@
+export class ApiError extends Error {
+	public status: number;
+	public statusText: string;
+	public body?: object | string;
+
+	constructor(res: Response, data: object, pathname: string) {
+		const { status, statusText } = res;
+		super(`ApiError: ${status} ${statusText} fetching ${pathname}`);
+		this.status = status;
+		this.statusText = statusText;
+
+		try {
+			this.body = data;
+		} catch (error) {}
+	}
+}
+export class ClientError extends ApiError {}
+export class ServerError extends ApiError {}
+export class OfflineError extends Error {}
+export class ServerConnectionError extends Error {}
--- app/utils/fetchInterceptor.ts
+++ app/utils/fetchInterceptor.ts
@@ -1,20 +1,56 @@
-import { cookies } from "next/headers";
+import { getCookie } from "./cookies";
+import { toast } from 'react-toastify';
+import { 
+	ServerConnectionError,
+	OfflineError,
+	ServerError,
+	ClientError
+} from './api/errors'
 
-const fetchInterceptor = async (url: string) => {
-  const jwt = await (await cookies()).get("jwt")
-  const data = await fetch(url, {
-    headers: {
-      Accept: "application/json",
-      Authorization:
-        "Bearer " + jwt?.value ,
-    },
-    cache: "no-store"
-  });
-  const res = await data.json();
-  return (res);
-}
+const fetchInterceptor = async (url: string, options?: RequestInit) => {
+	let res;
+	let data;
+	const { pathname } = new URL(url);
+	const resource = pathname.replace(/\/$/, '').split('/').pop();
 
-export default fetchInterceptor;
+	try {
+		const jwt = await getCookie("jwt");
+		res = await fetch(url, {
+			...options,
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+				Authorization: jwt ? "Bearer " + jwt : '',
+				...options?.headers
+			},
+			cache: "no-store"
+		});
+		data = await res.json();
+	} catch (error) {
+		console.error(`Fetch request failed ${url}`, error);
+		if (navigator.onLine) {
+			toast.error('Server seems to be offline');
+			throw new ServerConnectionError();
+		} else {
+			toast.error('No internet connection');
+			throw new OfflineError();
+		}
+	}
 
-export const fetchApi = (url: string) =>
-  fetchInterceptor(`${process.env.NEXT_PUBLIC_APIBASE}${url}`);
+	const status = res?.status;
+
+	if (status >= 500) {
+		toast.error(`Server error for ${resource}: ${res.status} ${res.statusText}`);
+		console.error('500 error', data, res);
+		throw new ServerError(res, data, pathname);
+	}
+
+	if (status >= 400) {
+		throw new ClientError(res, data, pathname);
+	}
+
+	return data;
+};
+
+export const fetchApi = (url: string, options?: RequestInit) =>
+	fetchInterceptor(`${process.env.NEXT_PUBLIC_APIBASE}${url}`, options);
