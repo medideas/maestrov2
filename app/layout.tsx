7f81906c049b6c967bf0bd166603a493d3ec01e3
Alexander Teglund <alexteg@gmail.com>
2024-12-21 14:29:43 +0100

    Remove unused profile fetch from layout
    No need to fetch the profile here, as the NavBar already fetches it and
    doesn't accept a `user` prop, so it's an unnecessary fetch and it does
    crash on the `/login` page when the token is missing or has expired.

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,13 +23,12 @@ export default async function RootLayout({
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	const user = await fetchApi("/my/profile/");
 	return (
 		<html lang="en" className="dm_sans.className">
 			<body className={"flex flex-col m-0 p-0"}>
 				<ToastContainer />
 				<Theme accentColor="red" radius="small" appearance="light">
-					<Navbar user={user} />
+					<Navbar />
 					<main className="flex-auto min-h-[80vh]">
 						<Suspense>{children}</Suspense>
 					</main>
